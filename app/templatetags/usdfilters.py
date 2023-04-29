0c524a557a80c1bdb2a4ec75b4def8ade10a77f0
MaheshBabu133 <maheshbabuntr123@gmail.com>
2023-04-29 11:26:00 +0530

    first commit

--- app/templatetags/usdfilters.py
+++ app/templatetags/usdfilters.py
@@ -1,17 +1,25 @@
 from django import template
 register=template.Library()
-def Swap(value):
+#this are by using the decorators
+@register.filter(name='Swaping') #using filter not same as function name
+def Swap(value):   #when we are using different name we need to menthon name in filter()
     return value.swapcase()
 
+@register.filter() #usign filter same as funtion name
 def CharCheck(value,arg):
     count=0
     for a in range(len(value)):
         if value[a:len(arg)+a]==arg:
             count=count+1
     return count
-
+@register.filter()
 def Remove(value,arg):
     return value.replace(arg,'')
+
+
+
+'''
+#this are the normal approach
 register.filter('Swaping',Swap)
 register.filter('CharCheck',CharCheck)
-register.filter('Remove',Remove)
+register.filter('Remove',Remove)'''
